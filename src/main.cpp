4c1762e9eb2333e2cfb54f3e8945f1df23a421ee
Daniel Soels <daniel.soels@edu.fh-joanneum.at>
2022-01-10 22:29:53 +0100

    REF changed to use connection_handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,25 @@
 #include "core/mqtt.h"
 #include "core/spiffs.h"
+#include "core/config.h"
 #include <Arduino.h>
 #include <WiFiClientSecure.h>
+#include "core/connection_handler.h"
 
-WiFiClientSecure tls_client;
 
-const char* server        = "abc.def@xyz.com";
-const char* Id            = "mein cooles esp device";
-const char* sub           = "asf";
-const char* pub           = "sadf";
-
-EspMqttConfig mqtt_config = {server, Id, sub, pub, 8333, tls_client};
 EspMqtt* esp_mqtt;
+EspConfig* esp_config;
 
+EspConnectionHandler esp_con_handler;
 void setup()
 {
   Serial.begin(9600);
+  
   esp_mqtt = EspMqtt::getInstance();
-  esp_mqtt->setup(mqtt_config);
+  esp_config = EspConfig::getInstance();
+  esp_config->readConfig();
+
+  esp_con_handler.setup();
+
 }
 
 void loop()
