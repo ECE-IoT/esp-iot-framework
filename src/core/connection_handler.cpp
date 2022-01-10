74f19e39f7ec7c6b860a15118e980437d0b72753
Daniel Soels <daniel.soels@edu.fh-joanneum.at>
2022-01-10 22:27:32 +0100

    ADD Setup function which setup mqtt, and wifi

--- src/core/connection_handler.cpp
+++ src/core/connection_handler.cpp
@@ -1,7 +1,39 @@
 #include "connection_handler.h"
 
-EspConnectionHandler(EspWifi* wifi_pointer, EspMqtt* mqtt_pointer)
+void EspConnectionHandler::setup()
 {
-  esp_wifi = wifi_pointer;
-  esp_mqtt = mqtt_pointer;
+  EspMqttConfig mqtt_config;
+  esp_mqtt_ = EspMqtt::getInstance();
+  esp_config_ = EspConfig::getInstance();
+
+  if(esp_mqtt_ == NULL)
+  {
+    // TODO include error handling (e.g. write to log file)
+    return;
+  }
+
+  if(esp_config_ == NULL)
+  {
+    // TODO include error handling (e.g. write to log file)
+    return;
+  }
+  
+  esp_wifi_.beginWifi(esp_config_->getWifiSSID() , esp_config_->getWifiPSWD());
+
+  while (esp_wifi_.statusWifi() != WL_CONNECTED)
+  {
+    /*TODO nochmal drüber denken und vl mit einer maximalen Zeit*/
+    /*Wird im Setup aufgerufen und würd die Lopp nicht blockieren*/
+  }
+
+  // fill struct for mqtt
+  mqtt_config.server_address = esp_config_->getAwsUrl();
+  mqtt_config.device_id = esp_config_->getDeviceID();
+  mqtt_config.subscribe_topic = esp_config_->getMqttSubTopic();
+  mqtt_config.publish_topic =  esp_config_->getMqttPubTopic();
+  mqtt_config.port =  esp_config_->getAwsPort();
+  mqtt_config.tls_client =  tls_client_;
+
+  esp_mqtt_->setup(mqtt_config);
+
 }
--- src/core/connection_handler.h
+++ src/core/connection_handler.h
@@ -2,21 +2,26 @@
 #define ESP_CONNECTION_HANDLER
 
 #include <Arduino.h>
+#include <WiFiClientSecure.h>
 #include "mqtt.h."
 #include "wifi.h"
+#include "config.h"
 
 enum connection_states{CONNECT_WIFI, CONNECT_MQTT, CONNECTED};
 
 class EspConnectionHandler
 {
   private:
-  EspWifi* esp_wifi;
-  EspMqtt* esp_mqtt = EspMqtt::getInstance();
+  EspWifi esp_wifi_;
+  EspMqtt* esp_mqtt_;
+  EspConfig* esp_config_;
+  WiFiClientSecure tls_client_;
 
-  connection_states state;
+  connection_states state_ = CONNECT_WIFI;
 
   public:
-  EspConnectionHandler(EspWifi* wifi_pointer, EspMqtt* mqtt_pointer);
+
+  void setup();
   connection_states runHandler();
 
   
