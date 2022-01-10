38f2e17d41f6875d490e02d2517733e79716af7a
Daniel Soels <daniel.soels@edu.fh-joanneum.at>
2022-01-10 14:30:27 +0100

    .

--- src/core/connection_handler.cpp
+++ src/core/connection_handler.cpp
@@ -1,1 +1,7 @@
 #include "connection_handler.h"
+
+EspConnectionHandler(EspWifi* wifi_pointer, EspMqtt* mqtt_pointer)
+{
+  esp_wifi = wifi_pointer;
+  esp_mqtt = mqtt_pointer;
+}
--- src/core/connection_handler.h
+++ src/core/connection_handler.h
@@ -2,11 +2,23 @@
 #define ESP_CONNECTION_HANDLER
 
 #include <Arduino.h>
+#include "mqtt.h."
+#include "wifi.h"
 
+enum connection_states{CONNECT_WIFI, CONNECT_MQTT, CONNECTED};
 
 class EspConnectionHandler
 {
-  
+  private:
+  EspWifi* esp_wifi;
+  EspMqtt* esp_mqtt = EspMqtt::getInstance();
+
+  connection_states state;
+
+  public:
+  EspConnectionHandler(EspWifi* wifi_pointer, EspMqtt* mqtt_pointer);
+  connection_states runHandler();
+
   
 };
 
