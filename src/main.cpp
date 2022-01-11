4b68e641785e3f8f1f8ea327462ed431783667fa
Daniel Soels <daniel.soels@edu.fh-joanneum.at>
2022-01-11 22:01:34 +0100

    changed for use with connection handler

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,22 @@ EspMqtt* esp_mqtt;
 EspConfig* esp_config;
 
 EspConnectionHandler esp_con_handler;
+
+connection_states state;
+
 void setup()
 {
   Serial.begin(9600);
   
-  esp_mqtt = EspMqtt::getInstance();
+  //esp_mqtt = EspMqtt::getInstance();
   esp_config = EspConfig::getInstance();
   esp_config->readConfig();
 
   esp_con_handler.setup();
-
 }
 
 void loop()
 {
+  state = esp_con_handler.runHandler();
+
 }
