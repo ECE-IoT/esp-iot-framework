c351f1cc5330df457fd59a72c5323e33543c3da1
t3mschm2d <tim.schmid1313@gmail.com>
2022-01-20 23:43:52 +0100

    ADD INIT to AWS!

--- src/core/sensor.cpp
+++ src/core/sensor.cpp
@@ -3,16 +3,18 @@
 EspSensor::EspSensor()
 {
   this->esp_mqtt_->getInstance();
+  this->esp_mqtt_->getInstance();
 }
 
 void EspSensor::setValue(float value)
 {
   this->current_value_ = value;
+  this->time           = this->esp_ntp_->getValue();
 }
 
 void EspSensor::updateMQTT()
 {
-  char* serialized_json_message =
-      this->esp_json_.serializeForSensor(this->current_value_, this->unit_, this->sensor_type_, this->unique_id_);
+  char* serialized_json_message = this->esp_json_.serializeForSensor(
+      this->current_value_, this->unit_, this->sensor_type_, this->unique_id_, this->time);
   this->esp_mqtt_->publish(this->mqtt_topic_, serialized_json_message);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,21 @@
+/**
+ * CORE INPUTS
+ */
 #include "core/config.h"
 #include "core/connection_handler.h"
 #include "core/json.h"
 #include "core/mqtt.h"
 #include "core/scheduler.h"
 #include "core/spiffs.h"
+/**
+ * SENSOR INPUTS
+ */
 #include "sensors/DHT22/DHT22.h"
 #include "sensors/LPS25/LPS25.h"
 #include "sensors/VEML7700/VEML7700.h"
-#include "sensors/SCD30/SCD30.h"
+/**
+ * LIBS
+ */
 #include <Arduino.h>
 #include <WiFiClientSecure.h>
 
@@ -21,49 +29,45 @@ EspJson esp_json;
 EspDHT22 esp_dht;
 EspLPS25 esp_lps25;
 EspVEML7700 esp_veml;
-EspSCD30 esp_scd30;
 
 EspMqtt* esp_mqtt;
 EspConfig* esp_config;
 
-void testPrint();
+void updateSensor();
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("Start");
 
-  esp_mqtt = EspMqtt::getInstance();
+  esp_mqtt   = EspMqtt::getInstance();
   esp_config = EspConfig::getInstance();
+
   esp_config->readConfig();
   esp_con_handler.setup();
-  esp_dht.setup(25);
-  esp_lps25.setup();
+
+  esp_dht.setup(4);
   esp_veml.setup();
-  esp_scd30.setup();
-  Serial.println("Start loop");
 }
 
 void loop()
 {
   state = esp_con_handler.runHandler();
-  // Serial.println(state);
   if (state == 2)
   {
-    // Serial.println("connected");
-    iotScheduler(2000, testPrint);
+    iotScheduler(2000, updateSensor);
   }
 }
 
-void testPrint()
+void updateSensor()
 {
+  /**
+   * READ THE SENSOR VALUE
+   */
   esp_dht.setValue();
-  esp_lps25.setValue();
   esp_veml.setValue();
-  esp_scd30.setValue();
+  /**
+   * PUBLISH SENSOR VALUE TO AWS
+   */
   esp_dht.update();
-  esp_lps25.update();
   esp_veml.update();
-  esp_scd30.update();
-  Serial.println("in callback");
 }
